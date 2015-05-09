0d737b64df7a0302430dd7e63786d666c6c5e8df
sanmaowang <wang@sanmao.me>
2015-05-09 21:56:09 +0800

    implement methods inheritance

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-function _Class(obj){
+function _Class(obj, parent){
 	var _constructor = function(){};
 
 	if(typeof obj.initialize === 'function'){
 		_constructor = obj.initialize;
+	}
+
+	if(typeof parent === 'function'){
+		var ctor = function(){};
+		ctor.prototype = parent.prototype;
+		_constructor.prototype = new ctor();
+		_constructor.prototype.constructor = _constructor;
+		_constructor.prototype.__super__ = parent.prototype;
 	}
 
 	for(var key in obj){
