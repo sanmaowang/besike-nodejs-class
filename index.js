f9fc8d1d945ce73c4c186e9affe21aec73e161c0
sanmaowang <wang@sanmao.me>
2015-05-09 21:33:46 +0800

    implement instance methods

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ function _Class(obj){
 		_constructor = obj.initialize;
 	}
 
+	for(var key in obj){
+		if(typeof obj[key] === 'function' && key !== 'initialize'){
+			_constructor.prototype[key] = obj[key];
+		}
+	}
+
 	return _constructor;
 
 };
